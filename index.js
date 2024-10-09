60861e36c6a4408b5740a642d28e77e758520d8a
jhan0058 <jhan0058@student.monash.edu>
2024-10-09 23:59:56 +0800

    Include New Multiple Bar Chart

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var visChart1 = "./json/net_population_changed_across_state.json";
 var visChart2 = "./json/comparision_of_births_deaths_in_states.json";
-var visChart3 = "./json/test2.json";
+var visChart3 = "./json/distribution_of_births_deaths_by_gender_in_states.json";
+var visChart4 = "./json/ratios_of_births_deaths_by_ethinic_group.json";
 
 vegaEmbed("#visChart1", visChart1)
   .then(function (result) {
@@ -15,6 +16,12 @@ vegaEmbed("#visChart2", visChart2)
   .catch(console.error);
 
 vegaEmbed("#visChart3", visChart3)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#visChart4", visChart4)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
