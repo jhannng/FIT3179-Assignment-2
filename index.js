9b31a3e7bb3ed426514e300d5171f3fe50303cfc
jhan0058 <jhan0058@student.monash.edu>
2024-10-09 18:52:19 +0800

    Include 2 Visualisation Chart

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-var geographical_distribution = "./json/geographical_distribution.json";
+var visChart1 = "./json/net_population_changed_across_state.json";
+var visChart2 = "./json/comparision_of_births_deaths_in_states.json";
+var visChart3 = "./json/test2.json";
 
-vegaEmbed("#geographical_distribution", geographical_distribution)
+vegaEmbed("#visChart1", visChart1)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#visChart2", visChart2)
+  .then(function (result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+  })
+  .catch(console.error);
+
+vegaEmbed("#visChart3", visChart3)
   .then(function (result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
   })
